ebb95b1751d1429a3ae1b79e5f5739bef44dfb86
harsh112904 <freelancer326014@gmail.com>
2025-12-02 12:09:28 +0530

    Update main.c

--- matchstickgame/src/main.c
+++ matchstickgame/src/main.c
@@ -1,107 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "../include/game.h"
-
- 
-
-void showRules() {
-    printf("\n◉ MATCHSTICK GAME\n");
-    printf("----------------------------------------\n");
-    printf("Total matchsticks: %d\n\n", TOTAL_MATCHSTICKS);
-    printf("Rules:\n");
-    printf("  - Pick between %d and %d matchsticks per turn\n", MIN_PICK, MAX_PICK);
-    printf("  - Player and computer alternate turns\n");
-    printf("  - Whoever takes the last matchstick loses\n\n");
-    printf("Good luck!\n\n");
-}
-
-int getComputerMove(int playerMove, int sticksLeft) {
-    if (sticksLeft <= 10) {
-        int coin = rand() % 4;
-        if (coin == 0) {
-            int tryPick = rand() % MAX_PICK + 1;
-            if (tryPick > sticksLeft) tryPick = clampValue(sticksLeft, MIN_PICK, MAX_PICK);
-            return tryPick;
-        }
-    }
-
-    int ideal = 10 - playerMove;
-    ideal = clampValue(ideal, MIN_PICK, MAX_PICK);
-
-    int wiggle = littleRandomizer(playerMove + sticksLeft) % 3;
-    if (wiggle == 1) {
-        return clampValue(ideal - 1, MIN_PICK, MAX_PICK);
-    }
-
-    return ideal;
-}
+#include "matchstick_game.h"
 
 int main() {
+    int matchsticks = TOTAL_MATCHSTICKS;
+    int playerTurn = 1;
+    int move;
+    int playerWins = 0, computerWins = 0;
+    char playAgain;
     
-    GameStateVerbose state;
-    state.sticksRemaining = TOTAL_MATCHSTICKS;
-    state.roundNumber = 0;
-    state.lastPlayerPick = 0;
+    srand(time(NULL));
 
-    int playerPick = 0;
-    int computerPick = 0;
+    printf("=== MATCHSTICK GAME (simple - Sem-1 style) ===\n");
+    printf("There are %d matchsticks. On each turn pick %d to %d.\n", TOTAL_MATCHSTICKS, MIN_PICK, MAX_PICK);
+    printf("Whoever picks the LAST matchstick loses.\n\n");
 
-    srand((unsigned int)time(NULL));
-    showRules();
+    do {
+        matchsticks = TOTAL_MATCHSTICKS;
 
-    while (state.sticksRemaining > 1) {
-        state.roundNumber++;
-        printf("\n----- Round %d -----\n", state.roundNumber);
-        printf("Sticks remaining: %d\n", state.sticksRemaining);
+        printf("Do you want to start first? (y/n): ");
+        char answer = 'y';
+        scanf(" %c", &answer);
 
+        if (answer == 'y' || answer == 'Y') {
+            playerTurn = 1;
+            printf("\nYou will start first.\n");
+        } else {
+            playerTurn = 0;
+            printf("\nComputer will start first.\n");
+        }
         
-        printf("Your turn - pick (%d-%d): ", MIN_PICK, MAX_PICK);
-        if (scanf("%d", &playerPick) != 1) {
-            int c; while ((c = getchar()) != '\n' && c != EOF) {}
-            printf("Invalid input. Please enter a number.\n");
-            continue;
+        while (matchsticks > 0) {
+            printf("\nRemaining matchsticks: %d\n", matchsticks);
+            
+            if (playerTurn) {
+                move = getUserMove(matchsticks);
+                printf("You picked %d matchstick(s)\n", move);
+            } else {
+                move = getComputerMove(matchsticks);
+                printf("Computer picked %d matchstick(s)\n", move);
+            }
+            
+            matchsticks -= move;
+            
+            if (matchsticks == 0) {
+                displayWinner(playerTurn);
+                if (playerTurn) {
+                    computerWins++;
+                } else {
+                    playerWins++;
+                }
+                break;
+            }
+            
+            playerTurn = !playerTurn;
         }
+        
+        displayGameStats(playerWins, computerWins);
 
-        if (!isValidMove(playerPick, state.sticksRemaining)) {
-            continue;
-        }
+        printf("\nPlay again? (y/n): ");
+        scanf(" %c", &playAgain);
 
-        state.lastPlayerPick = playerPick;
-        state.sticksRemaining = updateMatchsticks(state.sticksRemaining, playerPick);
-        printf("- You picked: %d\n", playerPick);
-        printf("- Remaining: %d\n", state.sticksRemaining);
-
-        if (state.sticksRemaining == TARGET_LEFT) {
-            announceWinner("player");
-            break;
-        }
-
-        if (state.sticksRemaining == 0) {
-            announceWinner("computer");
-            break;
-        }
-
-        computerPick = getComputerMove(playerPick, state.sticksRemaining);
-        if (computerPick > state.sticksRemaining) {
-            computerPick = clampValue(state.sticksRemaining, MIN_PICK, MAX_PICK);
-        }
-
-        printf("\nComputer picks: %d\n", computerPick);
-        state.sticksRemaining = updateMatchsticks(state.sticksRemaining, computerPick);
-        printf("- Remaining: %d\n", state.sticksRemaining);
-
-        if (state.sticksRemaining == TARGET_LEFT) {
-            announceWinner("computer");
-            break;
-        }
-
-        if (state.sticksRemaining == 0) {
-            announceWinner("player");
-            break;
-        }
-    }
-
-    printf("\nThank you for playing — goodbye!\n");
+    } while (playAgain == 'y' || playAgain == 'Y');
+    
+    printf("\nThanks for playing!\n");
     return 0;
 }
